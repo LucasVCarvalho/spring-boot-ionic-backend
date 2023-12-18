cd193b16b901114b6e0fc0928b738f2a7e99d67e
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2023-12-18 17:27:37 -0300

    Projeto criado

---
+++ src/main/java/com/licascarvalho/cursospringhiber/CursospringhiberApplication.java
@@ -0,0 +1,13 @@
+package com.licascarvalho.cursospringhiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringhiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringhiberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/licascarvalho/cursospringhiber/CursospringhiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licascarvalho.cursospringhiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringhiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
