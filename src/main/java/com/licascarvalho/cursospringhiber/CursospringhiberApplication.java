219a3fd5efb58c908cce7967b81fa2e588090083
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2023-12-26 18:04:51 -0300

    Criando operacao de instanciacao

--- src/main/java/com/licascarvalho/cursospringhiber/CursospringhiberApplication.java
+++ src/main/java/com/licascarvalho/cursospringhiber/CursospringhiberApplication.java
@@ -1,13 +1,34 @@
 package com.licascarvalho.cursospringhiber;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.licascarvalho.cursospringhiber.domain.Categoria;
+import com.licascarvalho.cursospringhiber.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringhiberApplication {
+public class CursospringhiberApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringhiberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
