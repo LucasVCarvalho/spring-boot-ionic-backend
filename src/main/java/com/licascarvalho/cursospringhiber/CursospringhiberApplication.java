d77ab4c55d4eef95251c19a34f39e8cbd0d568bc
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2023-12-28 15:36:44 -0300

    Estado e Cidade

--- src/main/java/com/licascarvalho/cursospringhiber/CursospringhiberApplication.java
+++ src/main/java/com/licascarvalho/cursospringhiber/CursospringhiberApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.licascarvalho.cursospringhiber.domain.Categoria;
+import com.licascarvalho.cursospringhiber.domain.Cidade;
+import com.licascarvalho.cursospringhiber.domain.Estado;
 import com.licascarvalho.cursospringhiber.domain.Produto;
 import com.licascarvalho.cursospringhiber.repositories.CategoriaRepository;
+import com.licascarvalho.cursospringhiber.repositories.CidadeRepository;
+import com.licascarvalho.cursospringhiber.repositories.EstadoRepository;
 import com.licascarvalho.cursospringhiber.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,7 +23,13 @@ public class CursospringhiberApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	private ProdutoRepository produtorepository;
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringhiberApplication.class, args);
@@ -43,7 +53,20 @@ public class CursospringhiberApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
-		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		
 	}
