ad8ecc5ad44ada8c18112e149d046a351fab6016
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2023-12-28 16:52:05 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/licascarvalho/cursospringhiber/repositories/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.licascarvalho.cursospringhiber.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.licascarvalho.cursospringhiber.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+	Optional<Cliente> findById(Integer id);
+	
+
+}
