63320ba4a29a6cfa183fcd983bf8d869f93de587
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2024-02-07 17:21:06 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/licascarvalho/cursospringhiber/resources/CategoriaResource.java
+++ src/main/java/com/licascarvalho/cursospringhiber/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -32,6 +33,19 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+	
+	//Exemplo de como vai funcionar a chamada /categorias/page?page=0&linesPerPage=24
+	//Recomenda linesPerPage=24 porquê é um numero multiplo de 1,2,3 e 4
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok(listDto);
 	}
 	
