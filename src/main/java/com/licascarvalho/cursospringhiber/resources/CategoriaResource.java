78ecd720fb83c8fbdba86536fa503f7529fe2a3b
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2024-02-07 16:37:11 -0300

    Atualizando Categoria com PUT, e outros pequenos ajustes

--- src/main/java/com/licascarvalho/cursospringhiber/resources/CategoriaResource.java
+++ src/main/java/com/licascarvalho/cursospringhiber/resources/CategoriaResource.java
@@ -19,17 +19,24 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok(obj);
 
 	}
-	
+	//void porquê quando for um sucesso é pra retornar nada
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
