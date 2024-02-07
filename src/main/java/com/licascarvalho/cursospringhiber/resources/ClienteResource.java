cae33ecf32b0092b864e0ed227c41ff43a85c0ab
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2024-02-07 19:15:12 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/licascarvalho/cursospringhiber/resources/ClienteResource.java
+++ src/main/java/com/licascarvalho/cursospringhiber/resources/ClienteResource.java
@@ -1,14 +1,28 @@
 package com.licascarvalho.cursospringhiber.resources;
 
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.licascarvalho.cursospringhiber.domain.Cliente;
+import com.licascarvalho.cursospringhiber.dto.ClienteDTO;
 import com.licascarvalho.cursospringhiber.services.ClienteService;
 
 @RestController
@@ -24,4 +38,49 @@ public class ClienteResource {
 		return ResponseEntity.ok(obj);
 
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+	
+	//Exemplo de como vai funcionar a chamada /categorias/page?page=0&linesPerPage=24
+	//Recomenda linesPerPage=24 porquê é um numero multiplo de 1,2,3 e 4
+	@GetMapping("/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok(listDto);
+	}
+	
+	//void porquê quando for um sucesso é pra retornar nada
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
+
