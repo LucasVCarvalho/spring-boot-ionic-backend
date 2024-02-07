fec5e17dd6f6eebc34a915fba716e5ca61b586a8
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2024-02-07 16:59:54 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/licascarvalho/cursospringhiber/resources/CategoriaResource.java
+++ src/main/java/com/licascarvalho/cursospringhiber/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.licascarvalho.cursospringhiber.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.licascarvalho.cursospringhiber.domain.Categoria;
+import com.licascarvalho.cursospringhiber.dto.CategoriaDTO;
 import com.licascarvalho.cursospringhiber.services.CategoriaService;
 
 @RestController
@@ -24,6 +27,14 @@ public class CategoriaResource {
 		return ResponseEntity.ok(obj);
 
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+	
 	//void porquê quando for um sucesso é pra retornar nada
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
